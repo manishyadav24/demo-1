da6cee48a30d69fb1826f7430f30010fc6553376
manishyadav24 <91630322+manishyadav24@users.noreply.github.com>
2022-03-17 10:30:43 +0530

    Add files via upload

---
+++ assets/js/expand-close-button.js
@@ -0,0 +1,26 @@
+
+  function myFunction() {
+  var y= document.getElementById("btn-hide");
+  
+    var x = document.getElementById("to-show");
+    if (x.style.display === "none") {
+      y.style.display = "none";
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function myFFunction(){
+  
+   var x = document.getElementById("btn-hide");
+     var y = document.getElementById("to-show");
+    if (x.style.display === "none") {
+    y.style.display = "none";
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  
+  }
+  
---
+++ assets/js/form.js
@@ -0,0 +1,14 @@
+function validate()
+{
+  var useremail= document.getElementById("youremail");
+  var usermessage= document.getElementById("yourmessage");
+
+  if(useremail.value=="" || usermessage.value=="")
+  {
+
+  }
+
+  else{
+    alert("Thank you for getting in touch! We appreciate you contacting us. One of our colleagues will get back in touch with you soon! Have a great day!");
+  }
+}
